a789851c88299e1bda46edd4ba6284cfe4cff8dc
sneham@klizos.com <sneham@klizos.com>
2025-02-14 19:09:16 +0530

    toast z-index increased

--- src/components/Toast.jsx
+++ src/components/Toast.jsx
@@ -21,7 +21,7 @@ const Toast = ({ message, type = 'info', duration = 3000, onClose }) => {
     }
 
     return (
-        <div className="toast toast-end z-50 w-full md:w-auto px-4 md:px-0">
+        <div className="toast toast-end z-50 w-full md:w-auto px-4 md:px-0" style={{zIndex: '9999999999999999999'}}>
             <div className={`alert alert-${type} flex gap-2 w-full md:w-auto`}>
                 {type === 'danger' && <IoWarningOutline className="w-5 h-5" />}
                 {type === 'success' && <IoCheckmarkCircleOutline className="w-5 h-5" />}
